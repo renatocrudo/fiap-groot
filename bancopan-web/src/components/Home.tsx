036261a40b770a4a4501063044554bc3af7e669f
Renato Crudo <re.crudo.85@gmail.com>
2022-06-19 18:22:04 -0300

    fix env

--- bancopan-web/src/components/Home.tsx
+++ bancopan-web/src/components/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
             <img src="./banco_pan.svg" alt="" />
             <h1>Projeto Banco Pan</h1>
             <div>
-                <p>Para acessar sua conta clique em <a href='/login'>Área do Cliente</a></p>
+                <p>Para acessar sua conta clique em <a href='/login'>Área do Cliente</a>!</p>
             </div>
         </div>
     )
