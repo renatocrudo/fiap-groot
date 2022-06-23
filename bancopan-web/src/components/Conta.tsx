2902858317e71310ff147e26251529a6ef82d827
Renato Crudo <re.crudo.85@gmail.com>
2022-06-22 22:04:48 -0300

    adjusts front

--- bancopan-web/src/components/Conta.tsx
+++ bancopan-web/src/components/Conta.tsx
@@ -33,14 +33,46 @@ function Conta() {
 
 
     return (
-        <div>
-            <h1>Olá Renato</h1>
+        <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+                    <div>
+
+            <nav className="navbar navbar-default navbar-fixed-top">
+  <div className="container">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>                        
+      </button>
+      <a className="navbar-brand" href="#myPage"><img src="./banco_pan.svg" alt="" /></a>
+    </div>
+    <div className="collapse navbar-collapse" id="myNavbar">
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#about">Pagamentos</a></li>
+        <li><a href="#services">Investimentos</a></li>
+        <li><a href="#portfolio">Seguros</a></li>
+        <li><a href="#pricing"> </a></li>
+        <li><a href="/">Sair</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
             <div>
                 <a href="#">Entradas</a> &nbsp;<a href="#">Shopping</a> &nbsp;<a href="#">Cartão</a> &nbsp;
             </div>
             <div> &nbsp; </div>
-            <div>
-                <table>
+ <div id="about" className="container-fluid">
+        <div className="row">
+            <div className="col-sm-8">
+            <h2>Olá Renato</h2><br/>
+            <h4>Veja abaixo seu extrato da conta corrente.</h4><br/>
+            </div>
+            <div className="col-sm-4">
+            <span className="glyphicon glyphicon-signal logo"></span>
+            </div>
+        </div>
+        <div className='table-reponsive'>
+                <table className='table table-hover'>
                     <thead>
                         <tr>
                             <th>Data      </th>
@@ -54,19 +86,23 @@ function Conta() {
                     {contas.map(conta => {
                         var isDespesa = conta.cd_tipo == '0';
                         return (                        
-                            <tr key={conta.id} style={isDespesa ? {color:'red'}: {color:'blue'}}>
+                            <tr key={conta.id}>
                                 <td>{conta.data}</td>
                                 <td></td>
                                 <td>{conta.descricao}</td>
                                 <td></td>
-                                <td>{ isDespesa ? "-":""} R$ {conta.valor}</td>
+                                <td style={isDespesa ? {color:'red'}: {color:'blue'}}>{ isDespesa ? "-":""} R$ {conta.valor}</td>
                             </tr>
                         )
                     })}
                     </tbody>
                 </table>
             </div>
+</div>
+
         </div>
+
+        </body>
     )
 }
 
